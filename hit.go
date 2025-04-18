de51b1613bf80b54c1b293c6d3058d0e827ec7a7
Talha Waheed <talhawaheed57@gmail.com>
2025-04-18 23:04:31 +0000

    changed default timeout to 5s

--- hit.go
+++ hit.go
@@ -188,7 +188,7 @@ func makeReqToEndpoint(
 	// }
 	// res, err := client.Do(req)
 	client := &http.Client{
-		Timeout: 10 * time.Second, // Set a timeout for the entire request
+		Timeout: 5 * time.Second, // Set a timeout for the entire request
 	}
 	res, err := client.Do(req)
 	latency := time.Since(startReq)
